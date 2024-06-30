2c0cfad17d47d477e2b1b0943a0818734a219ccc
Sujan <mail.thapasujan@gmail.com>
2024-06-30 21:24:39 +1000

    Some header css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,41 @@
 import React from "react";
-import { Container, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Logo } from "./Logo";
+import { FaEnvelope, FaHome, FaShoppingBag, FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
     <Navbar expand="lg" className="nav-bg">
       <Container>
-        <Navbar.Brand href="#home">Chautari</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <Logo />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        {/* <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-              </Nav>
-            </Navbar.Collapse> */}
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Link className="nav-link icon-link" to="/">
+              <FaHome className="icon" />
+              <span className="link-label">Home</span>
+            </Link>
+            <Link className="nav-link icon-link" to="/shop">
+              <FaShoppingBag className="icon" />
+              <span className="link-label">Shop</span>
+            </Link>
+            {/* <Link className="nav-link icon-link">
+              <FaPlus className="icon" style={{ color: "red" }} />
+              <span className="link-label">Add</span>
+            </Link> */}
+            <Link className="nav-link icon-link" to="/messages">
+              <FaEnvelope className="icon" />
+              <span className="link-label">Messages</span>
+            </Link>
+            <Link className="nav-link icon-link" to="/me">
+              <FaUser className="icon" />
+              <span className="link-label">Me</span>
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
---
+++ src/components/Logo.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import logo from "../assets/logo.jpg";
+
+export const Logo = () => {
+  return <img src={logo} alt="Logo" style={{ width: "60px" }} />;
+};
